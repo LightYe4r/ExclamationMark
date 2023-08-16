c587d8e4df87ae57d34cd06acad5c05e9ab8228d
LightYe4r <24adrenalin@naver.com>
2023-08-16 16:28:12 +0900

    FIX : CSRF ERROR

--- Exclamation_mark/Exclamation_mark/settings.py
+++ Exclamation_mark/Exclamation_mark/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.Allow_Any',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
